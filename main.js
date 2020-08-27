f938a1de96d0116eaaf8aa16f610bf97094e5639
Mauricio Fabian Bayas Velez <34218019+mbayas21@users.noreply.github.com>
2020-08-27 17:09:15 -0400

    Add files via upload
    First Commit

---
+++ main.js
@@ -0,0 +1,113 @@
+// function orderMyLogic(val) {
+//     if(val < 10 && val > 5) {
+//       return "Less than 10";
+//     }
+//     else if(val < 5)
+//       return "Less than 5";
+//     else {
+//         return "Greater than or equal to 10";
+//       }  
+// }
+
+// var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+// function golfScore(par, strokes) {
+//   // Only change code below this line
+//     if (strokes == 1) {
+//       return names[0];
+//     }
+//     else if(strokes <= par - 2) {
+//       return names[1]
+//     }
+//     else if(strokes == par - 1) {
+//       return names[2]
+//     }
+//     else if(strokes == par) {
+//       return names[3]
+//     }
+//     else if(strokes == par + 1) {
+//       return names[4]
+//     }
+//     else if(strokes == par + 2) {
+//       return names[5]
+//     }
+//     else if(strokes >= par + 3) {
+//       return names[6]
+//     }
+//     else{
+//       return "Change Me";
+//     }
+  
+//   // Only change code above this line
+// }
+
+// var count = 0;
+// function cc(card) {
+//   switch(card) {
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//     case 6:
+//       count++;
+//       break;
+//     case 10:
+//     case "J":
+//     case "Q":
+//     case "K":
+//     case "A":
+//       count--;
+//       break;
+//   }
+
+//   var holdbet = 'Hold'
+//   if (count > 0) {
+//     holdbet = 'Bet'
+//   }
+// }
+
+// var testing = "testing"
+// function kebabToSnake() {
+//   return kebab.replace(/-/g, "_");
+// }
+
+// function factorial(n){
+//   var result = 1;
+//   for(var i = 2; i <= n; i++){
+//           result =  result * i;
+//   }
+//   return result;
+// }
+
+// factorial(3);
+
+// function testFunc(){
+//   document.getElementById("events-listing").innerHTML = "Changed this shit";
+// }
+
+// var fakeObject = {
+//   gift: "pony",
+//   pet: "kitten",
+//   bed: "sleigh",
+// }
+
+// function checkObj(obj, checkProp) {
+//   if(obj.hasOwnProperty(checkProp)){
+//     return obj[checkProp];
+//   } else {
+//     return "Not Found";
+//   }
+// }
+
+// let names = ['Chris', 'Li Kang', 'Anne', 'Francesca', 'Mustafa', 'Tina', 'Bert', 'Jada']
+// let para = document.createElement('p');
+
+// // Add your code here
+// function chooseName(){
+//   var randomNum = Math.floor(Math.random() * names.length);
+//   return names[randomNum];
+// }
+// Don't edit the code below here!
+
+// section.innerHTML = ' ';
+
+// section.appendChild(para);
